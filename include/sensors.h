6ba3cfe3f6795249eb415295b1a66f4d4d76230f
Randa-Almadhoun <randa.almadhoun@gmail.com>
2016-05-02 17:02:22 +0400

    - modifications to add multi sensor functionality per waypoint and generate SS (still without filtering)

---
+++ include/sensors.h
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *   Copyright (C) 2006 - 2016 by                                          *
+ *      Tarek Taha, KURI  <tataha@tarektaha.com>                           *
+ *      Randa Almadhoun   <randa.almadhoun@kustar.ac.ae>                   *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ ***************************************************************************/
+#ifndef SENSORS_H_
+#define SENSORS_H_
+#include <iostream>
+#include <math.h>
+#include "ros/ros.h"
+#include <ros/package.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/tf.h>
+#include <tf_conversions/tf_eigen.h>
+#include <geometry_msgs/Pose.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <geometry_msgs/PoseArray.h>
+#include "tf/transform_listener.h"
+#include "sensor_msgs/PointCloud.h"
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "nav_msgs/Odometry.h"
+#include <deque>
+#include <std_msgs/Bool.h>
+#include <cmath>
+#include <math.h>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <cassert>
+#include <iomanip>
+#include "fcl/math/transform.h"
+
+using namespace fcl;
+using namespace std;
+class Sensors
+{
+public :
+    std::string robotModel,robotName;
+    double hFOV, vFOV, focalLength, near, far, resWidth, resHeight;// sensorSize;
+//    geometry_msgs::Pose sensorLocation; //with respect to the UAV
+    Vec3f sensorPose, sensorRPY;
+    Sensors(double hfov, double vfov, double fl, double nearR, double farR, double width, double height, Vec3f sP, Vec3f sRPY);
+    geometry_msgs::Pose uav2camTransformation(geometry_msgs::Pose pose);
+    Sensors();
+    ~Sensors();
+};
+
+#endif /*SENSORS_H_*/
---
+++ src/sensors.cpp
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *   Copyright (C) 2006 - 2016 by                                          *
+ *      Tarek Taha, KURI  <tataha@tarektaha.com>                           *
+ *      Randa Almadhoun   <randa.almadhoun@kustar.ac.ae>                   *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ ***************************************************************************/
+#include "sensors.h"
+
+Sensors::Sensors(double hfov, double vfov, double fl, double nearR, double farR, double width, double height, Vec3f sP, Vec3f sRPY):
+hFOV(hfov),
+vFOV(vfov),
+focalLength(fl),
+near(nearR),
+far(farR),
+resWidth(width),
+resHeight(height),
+sensorPose(sP),
+sensorRPY(sRPY)
+{
+
+}
+
+
+Sensors::Sensors()
+{
+}
+
+Sensors::~Sensors()
+{
+}
+
+geometry_msgs::Pose Sensors::uav2camTransformation(geometry_msgs::Pose pose)
+{
+
+
+    Eigen::Matrix4d uav_pose, uav2cam, cam_pose;
+    //UAV matrix pose
+    Eigen::Matrix3d R; Eigen::Vector3d T1(pose.position.x,pose.position.y,pose.position.z);
+    tf::Quaternion qt(pose.orientation.x,pose.orientation.y,pose.orientation.z,pose.orientation.w);
+    tf::Matrix3x3 R1(qt);
+    tf::matrixTFToEigen(R1,R);
+    uav_pose.setZero ();
+    uav_pose.block (0, 0, 3, 3) = R;
+    uav_pose.block (0, 3, 3, 1) = T1;
+    uav_pose (3, 3) = 1;
+
+    //transformation matrix
+    qt = tf::createQuaternionFromRPY(sensorRPY[0],sensorRPY[1],sensorRPY[2]);
+    tf::Matrix3x3 R2(qt);Eigen::Vector3d T2(sensorPose[0], sensorPose[1], sensorPose[2]);
+    tf::matrixTFToEigen(R2,R);
+    uav2cam.setZero ();
+    uav2cam.block (0, 0, 3, 3) = R;
+    uav2cam.block (0, 3, 3, 1) = T2;
+    uav2cam (3, 3) = 1;
+
+    //preform the transformation
+    cam_pose = uav_pose * uav2cam;
+
+    Eigen::Matrix4d cam2cam;
+    //the transofrmation is rotation by +90 around x axis of the camera
+    cam2cam <<   1, 0, 0, 0,
+                 0, 0,-1, 0,
+                 0, 1, 0, 0,
+                 0, 0, 0, 1;
+    Eigen::Matrix4d cam_pose_new = cam_pose * cam2cam;
+    geometry_msgs::Pose p;
+    Eigen::Vector3d T3;Eigen::Matrix3d Rd; tf::Matrix3x3 R3;
+    Rd = cam_pose_new.block (0, 0, 3, 3);
+    tf::matrixEigenToTF(Rd,R3);
+    T3 = cam_pose_new.block (0, 3, 3, 1);
+    p.position.x=T3[0];p.position.y=T3[1];p.position.z=T3[2];
+    R3.getRotation(qt);
+    p.orientation.x = qt.getX(); p.orientation.y = qt.getY();p.orientation.z = qt.getZ();p.orientation.w = qt.getW();
+
+    return p;
+
+}
